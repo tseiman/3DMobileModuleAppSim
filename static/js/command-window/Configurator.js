e183d587bb3090601dd35bfdc242d0df2365e6ee
Thomas Schmidt <t.schmidt@md-network.de>
2022-05-14 01:35:05 +0200

    added config up/download

--- static/js/command-window/Configurator.js
+++ static/js/command-window/Configurator.js
@@ -7,9 +7,11 @@ class Configurator {
 		this.config = {};
 		this.tempConfig = {};
 
+
 		var that = this;
 
-		if (localStorage.getItem("suitcase-config") !== null) {
+
+		if ((window.localStorage.getItem("suitcase-config")) && (window.localStorage.getItem("suitcase-config") !== null)) {
 			this.config = JSON.parse(window.localStorage.getItem("suitcase-config"));
 		}
 
@@ -19,24 +21,12 @@ class Configurator {
 				<i class="close icon" style="font-family: 'Icons'!important;"></i>
 				<div class="header" style="border-bottom: 1px solid rgba(34,36,38,.15); border-radius: 0.285714rem;">Configuration
 	
-<!--				<button class="circular ui icon button" style="float: right;" id="btn-config-download">
-					  <i class="icon download"></i>
-					</button>
-
-
-<div class="ui icon big button" id="divUpload">
-        <i class="cloud icon"></i>
-        
- </div>
-<input type="file" id="hidde-new-file" style="display: none">
-
-
-					
-					<button class="circular ui icon button"  for="embedpollfileinput" style="float: right;" id="btn-config-upload">
-					  <i class="icon upload"></i>
-					         <input type="file" (change)="fileEvent($event)" class="inputfile" id="embedpollfileinput" />
-					</button> 
--->
+					<button class="circular ui icon button" style="float: right;" id="btn-config-download">
+						  <i class="icon download"></i>
+						</button>
+					<button class="circular ui icon button" style="float: right;" id="btn-config-upload">
+						  <i class="icon upload"></i>
+						</button>
 
 				</div>
 				<div class="scrolling content">
@@ -50,23 +40,59 @@ class Configurator {
 					</div>
 				</div>
 			</div>
+			<div class="ui large modal" id="upload-configuration-modal" style="border-radius: .28571429rem;">
+				<i class="close icon" style="font-family: 'Icons'!important;"></i>
+				<div class="header" style="border-bottom: 1px solid rgba(34,36,38,.15); border-radius: 0.285714rem;">Enter Configuration</div>
+				<div class="content">
+					<div class="ui form" id="config-upload-form">
+						<div class="field button" id="upload-config-item">
+							<label>Upload Config</label><textarea name="upload-config-item" id="upload-config-item-input" placeholder="your config here ..."  data-content="Please enter a valid configuration JSON"></textarea>
+						</div>
+					</div>
+				</div>
+				<div class="actions" style="border-top: 1px solid rgba(34,36,38,.15); border-radius: 0.285714rem;">
+					<div class="ui negative button centered"  id="btn-config-upload-Nok">Cancel</div>
+					<div class="ui positive right labeled icon button" style="font-family: Roboto, Arial, sans-serif !important; background-color: #21ba45;" id="btn-config-upload-ok">
+						OK
+						<i class="checkmark icon" style="top: -4px; background-color: rgba(0,0,0,.05);"></i>
+					</div>
+				</div>
+			</div>
+
+
 
 		`);
 
+		$("#btn-config-upload").click((e) => {
+			$('#upload-configuration-modal').modal('show');
+		});
+		$("#btn-config-upload-Nok").click((e) => {
+			$('#configuration-modal').modal('show');
+		});
+		$("#btn-config-upload-ok").click((e) => {
+			console.log($('#upload-config-item-input').val());
+			var tmpConf = JSON.parse($('#upload-config-item-input').val());
 
+			if(tmpConf && (Object.keys(tmpConf).length >= 0)) {
+				this.tempConfig =tmpConf;
+//				console.log(that.config);
+//				window.localStorage.setItem("suitcase-config", JSON.stringify(that.config));
+				this.saveConfig();
+			//	location.reload();
+			}
+		});
 
-		
-
-		$("#btn-config-ok").click(function(e) {
+		$("#btn-config-ok").click((e) => {
 			that.saveConfig();
 
 		});
 
 
-		$("#btn-config-download").click(function() {
+		$("#btn-config-download").click((e) => {
+			that.config.id= Date.now();
   			$('<a/>', {
     			"download": "sierrademo-config.json",
-   				 "href" : "data:application/json," + encodeURIComponent(JSON.stringify(that.config)),
+   				 "href" : "data:application/json," + encodeURIComponent(JSON.stringify(that.config, null, 2)),
   			}).appendTo("body").click(function() {
      			$(this).remove();
 			})[0].click();
@@ -183,7 +209,7 @@ class Configurator {
 	saveConfig() {
 		this.config = this.tempConfig;
 		window.localStorage.setItem("suitcase-config", JSON.stringify(this.config));
-
+		location.reload();
 //		console.log("save Config:", this.tempConfig);
 	}
 
